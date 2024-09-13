8c66b271266aa3b21a3dd6ad7ab3da6cbac7661d
HUNAIN882 <hunaintoufeeq@gmail.com>
2024-09-13 16:27:21 -0700

    Add new paragraph

---
+++ app.js
@@ -0,0 +1,90 @@
+// Start Assignment No# 1 (Chapter 1-8)
+
+
+let firstname, lastname;
+firstname = "Muhammad"
+lastname = "Hunain"
+var address = "main street"
+address = "First Street"
+
+console.log(firstname);
+console.log(lastname);
+console.log(address);
+
+// End Assignment No# 1 (Chapter 1-8)
+
+
+
+// Start Assignment No# 2 (Chapter 9-11)
+
+let street = mainstreet = "Gurumandir"
+let country = "Pakistan"
+
+let maillingAddress = street + " " + country;
+
+console.log(maillingAddress)
+
+
+// End Assignment No# 2 (Chapter 9-11)
+
+
+
+// Start Assignment No# 3 (12-13)
+
+const score1 = 50;
+const score2 = 20;
+const score3 = 30;
+
+let totalScore = "Our Total Score is" + "" + (score1 + score2 + score3)
+
+let avgScore = "Our Average Score is" + "" + (score1 + score2 + score3 / 3)
+
+let plates = 20;
+let people = 7;
+
+let remaingPlates = plates % people;
+
+remaingPlates + 1 ;
+
+let message = `There are ${remaingPlates} plates avaiable `
+
+console.log (totalScore);
+console.log (avgScore);
+console.log (remaingPlates);
+console.log (message);
+
+
+// End Assignment No# 3 (12-13)
+
+
+
+// Start Assignment No# 4 (Chapter 14-16)
+
+let fruits = ["apple" , "orange" ,"Peach" , "Mango" , 42]
+console.log(fruits);
+let istFruit = fruits [0];
+console.log(istFruit);
+fruits[4] = "grapes"
+console.log(fruits);
+
+function calculateTotal(subTotal, tax) {
+    return subTotal + tax;
+}
+
+let order1, order2, order3;
+order1 = calculateTotal(100, 20);
+order2 = calculateTotal(200, 30);
+order3 = calculateTotal(400, 50);
+
+
+
+console.log("Order 1 Total:" + order1);
+console.log("Order 2 Total:" + order2);
+console.log("Order 3 Total:" + order3);
+// End Assignment No# 4 (Chapter 14-16)
+
+
+
+
+
+
